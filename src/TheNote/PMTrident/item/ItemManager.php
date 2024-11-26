16ae0604468d7c254186a2a9ded324693841b352
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2024-11-27 06:28:27 +0700

    Update ItemManager.php

--- src/TheNote/PMTrident/item/ItemManager.php
+++ src/TheNote/PMTrident/item/ItemManager.php
@@ -5,8 +5,9 @@ declare(strict_types = 1);
 namespace TheNote\PMTrident\item;
 
 use pocketmine\item\ItemIdentifier;
-use pocketmine\item\ItemIds;
-use pocketmine\item\ItemFactory;
+use pocketmine\item\ItemTypeIds as ItemIds;
+use pocketmine\item\StringToItemParser;
+use pocketmine\world\format\io\GlobalItemDataHandlers;
 
 class ItemManager {
     public static function init(): void
