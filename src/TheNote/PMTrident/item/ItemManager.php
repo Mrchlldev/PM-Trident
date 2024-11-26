5d9c430e4a26269c31dc05096c5995d445ece5f8
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2024-11-27 06:38:38 +0700

    Update ItemManager.php

--- src/TheNote/PMTrident/item/ItemManager.php
+++ src/TheNote/PMTrident/item/ItemManager.php
@@ -4,10 +4,12 @@ declare(strict_types = 1);
 
 namespace TheNote\PMTrident\item;
 
+use TheNote\PMTrident\item\ExtraVanillaItems;
 use pocketmine\item\ItemIdentifier;
 use pocketmine\item\ItemTypeIds as ItemIds;
 use pocketmine\item\StringToItemParser;
 use pocketmine\world\format\io\GlobalItemDataHandlers;
+use pocketmine\data\bedrock\item\ItemTypeNames;
 use pocketmine\inventory\CreativeInventory;
 
 class ItemManager {
@@ -17,6 +19,12 @@ class ItemManager {
         $itemSerializer = GlobalItemDataHandlers::getSerializer();
         $creativeInventory = CreativeInventory::getInstance();
         $stringToItemParser = StringToItemParser::getInstance();
+
+        $trident = ExtraVanillaItems::TRIDENT();
+        $itemDeserializer->map(ItemTypeNames::TRIDENT, static fn() => clone $trident);
+        $itemSerializer->map($trident, static fn() => new SavedItemData(ItemTypeNames::TRIDENT));
+        $creativeInventory->add($trident);
+        $stringToItemParser->register("trident", static fn() => clone $trident);
         //ItemFactory::getInstance()->register(new Trident(new ItemIdentifier(ItemIds::TRIDENT, 0), "Trident"));
     }
 }
